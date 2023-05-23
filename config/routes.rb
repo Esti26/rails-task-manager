11e4f206ce31613d6eaa87a6ee1bda1d746cb698
Estibaliz Lorenzo <esti_2612@hotmail.com>
2023-05-23 16:55:37 +0200

    adds a list that can be updated, edited and deleted

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: 'create'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  get 'tasks/:id', to: 'tasks#show', as: 'show'
+  patch 'tasks/:id', to: 'tasks#update', as: 'task'
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy'
 end
